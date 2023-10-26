ca1a979611280bc77ffb25c363fb05777a76fee3
Stephen Klop <s.klop@xitra.nl>
2023-10-26 12:03:29 +0200

    Changed logo link

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -7,7 +7,7 @@ const Header = (props: any) => {
             <div className="container flex flex-wrap items-center justify-center mx-auto">
                 <a href="https://ismsbv.nl" className="flex items-center">
                     <Image
-                        src="https://aysa.ams3.cdn.digitaloceanspaces.com/aysa-logo.png"
+                        src="https://aysa.ams3.cdn.digitaloceanspaces.com/aysa-logo-v1.png"
                         width={124}
                         height={40}
                         alt="ISMS Logo"
